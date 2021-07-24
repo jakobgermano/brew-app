164c0cae21d6d1b238827fb5e0f647b320e02b51
jakob <jakob.germano@gmail.com>
2021-07-24 12:41:32 -0600

    1st commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const MAIN_URL = `https://api.openbrewerydb.org`
+
+ document.addEventListener('DOMContentLoaded', () =>{
+    //document.getElementById('brews').addEventListener('click', grabBreweries)
+    grabBreweries()
+})
+
+function grabBreweries() {
+    let main = document.getElementById('main ul') //putting those elements from the html into variables to use later in the function
+        let brewList = document.getElementById('brew-list')
+        let info = document.getElementById('info')
+    fetch(`https://api.openbrewerydb.org/breweries`)
+        .then(res => res.json())
+        .then(brewies => { 
+            brewies.map(brew => {
+                brewList.innerHTML += `
+                    <li>
+                        ${brew.name}
+                    </li>    
+                `
+            
+            })
+
+        })
+    
+}
