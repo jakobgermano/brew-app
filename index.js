d86fc1dba6597d15b2cf5a2e1dc95f29ae563d59
jakob <jakob.germano@gmail.com>
2021-07-27 19:32:12 -0600

    added picture

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const MAIN_URL = `https://api.openbrewerydb.org`
-
  document.addEventListener('DOMContentLoaded', () =>{
     document.getElementById('brews').addEventListener('click', grabBreweries)
     grabBreweries()
 })
 
 function grabBreweries() {
-    let main = document.getElementById('main ul') //putting those elements from the html into variables to use later in the function
+     
         let brewList = document.getElementById('brew-list')
         let info = document.getElementById('info')
         info.innerHTML = ""
@@ -28,6 +27,7 @@ function grabBreweries() {
 }
 
 function linkClicks() {
+
 const favButtons = document.querySelectorAll('button.buttons')
 favButtons.forEach(brew => {
     brew.addEventListener('click', favBrew)
@@ -41,11 +41,12 @@ const brewies = document.querySelectorAll('li a')
 }
 
 function favBrew(e){
-    //console.log(e.target.parentElement)
+
     const li = e.target.parentElement
     const button = e.target
     const likes = li.querySelector('p')
-    button.dataset.likes ++ 
+        button.dataset.likes ++ 
+
     if (likes === null ) {
         li.insertAdjacentHTML('beforeend', `<p id ='likes'> favs: ${button.dataset.likes}</p>`)
     }else {
@@ -55,13 +56,14 @@ function favBrew(e){
 }
 
 async function showBrewery(e) {
-    console.log(e.target)
-    let info = document.getElementById('info')
-    let brewList = document.getElementById('brew-list')
+    
+     let info = document.getElementById('info')
+     let brewList = document.getElementById('brew-list')
     brewList.innerHTML = ""
     fetch(`https://api.openbrewerydb.org/breweries/${e.target.dataset.id}`)
     .then(res => res.json())
     .then(brew => {
+        
         info.innerHTML += `
             <h1>${brew.name}</h1>
             <h3>Location:</h3>
